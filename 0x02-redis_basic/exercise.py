7a0000e9233985c9fe20a0bf00cda38878481f49
waltertaya <waltermittybecker5@gmail.com>
2024-06-19 19:46:32 +0300

    Added: Task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" exercise.py file
+"""
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Cache class for storing data in Redis with a random key"""
+    
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    
+    def store(self, data: Union[str, int, bytes, float]) -> str:
+        """Generate a random key and store the input data in Redis"""
+        rand = str(uuid.uuid4())
+        self._redis.set(rand, data)
+        return rand
